85e8bd3bd50956f748824e7e656e8b96a086a690
max.nash@lexicondigital.com.au <max.nash@lexicondigital.com.au>
2022-09-19 16:03:45 +1000

    grid & img assets

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//if id has class == alert `you cannot click here` (this is top in read order)
+//event listener, on click if player-1 = true, append chicken class, if player-2 = true, append bin class
+//if id topleft && top center && top right = class chicken === winner, player 1
+//same logic for center left, middle, center right
+//top left, center left, bottom left
+//top right, center right, bottom right
+//top center, middle, bottom center
+//top left, middle, bottom right
+//top right, middle, bottom left
