0338cb9ee31421646af0d536af0a38c47309997e
max.nash@lexicondigital.com.au <max.nash@lexicondigital.com.au>
2022-09-21 10:45:09 +1000

    winning logic

--- js/script.js
+++ js/script.js
@@ -4,25 +4,20 @@
 
 
 //if id topleft && top center && top right = class chicken === winner, player 1
-//same logic for center left, middle, center right
-//top left, center left, bottom left
-//top right, center right, bottom right
-//top center, middle, bottom center
-//top left, middle, bottom right
-//top right, middle, bottom left
+
 
 // const player1 = firstclick
 
-// let currentPlayer = player1
-// console.log(currentPlayer)
-//`Player 1's Turn`
-//const playerAnnounce = document.querySelector(`somethingHere`)
-// if (currentPlayer = player1){
-// playerAnnounce.textContent = `Player 1's Turn`
-// }
-//else {
-// playerAnnounce.textContent = `Player 2's Turn`
-// }
+let currentPlayer = player1
+console.log(currentPlayer)
+`Player 1's Turn`
+const playerAnnounce = document.querySelector(`somethingHere`)
+if (currentPlayer = player1){
+playerAnnounce.textContent = `Player 1's Turn`
+}
+else {
+playerAnnounce.textContent = `Player 2's Turn`
+}
 
 //Selector Logic - Apply Bin or Chicken class on DivClick
 const TopRightGrab = document.querySelector(`#top-right`)
@@ -152,11 +147,50 @@ BottomCenterGrab.addEventListener(`click`, () =>{
     }
 })
 
-// if (TopLeftGrab.className === TopRightGrab.className && TopCenterGrab.className === TopCenterGrab.className){
-    // return winner screen
-    //`${TopLeftGrab.className}'s Win!` as text in a header / wrapped page content/ centred
-// }
+//same logic for center left, middle, center right!
+//top left, center left, bottom left
+//top right, center right, bottom right
+//top center, middle, bottom center
+//top left, middle, bottom right
+//top right, middle, bottom left
 
+if (TopLeftGrab.className === TopRightGrab.className && TopLeftGrab.className === TopCenterGrab.className){
+    return winner
+    `${TopLeftGrab.className}'s Win!` //as text in a header / wrapped page content/ centred
+}
+if (CenterLeftGrab.className === middleGrab.className && CenterLeftGrab.className === CenterRightGrab.className){
+    return winner
+    `${CenterLeftGrab.className}'s Win!` 
+}
+if (BottomLeftGrab.className === BottomRightGrab.className && BottomLeftGrab.className === BottomCenterGrab.className){
+    return winner
+    `${BottomLeftGrab.className}'s Win!`
+}
+
+if (TopLeftGrab.className === CenterLeftGrab.className && TopLeftGrab.className === BottomLeftGrab.className){
+    return winner
+    `${TopLeftGrab.className}'s Win!`
+}
+
+if (TopCenterGrab.className === middleGrab.className && TopCenterGrab.className === BottomCenterGrab.className){
+    return winner
+    `${TopCenterGrab.className}'s Win!`
+}
+
+if (TopRightGrab.className === CenterRightGrab.className && TopRightGrab.className === BottomRightGrab.className){
+    return winner
+    `${TopRightGrab.className}'s Win!`
+}
+
+if (TopLeftGrab.className === middleGrab.className && TopLeftGrab.className === BottomRightGrab.className){
+    return winner
+    `${TopLeftGrab.className}'s Win!`
+}
+
+if (TopRightGrab.className === middleGrab.className && TopRightGrab.className === BottomLeftGrab.className){
+    return winner
+    `${TopRightGrab.className}'s Win!`
+}
 //End of Selector Logic
 
 //Player Logic
@@ -171,3 +205,5 @@ BottomCenterGrab.addEventListener(`click`, () =>{
 //     playerIndicator.textContent = `Player 2's Turn`
 // }
 
+//restart button = refresh page /reload
+//score - between games
